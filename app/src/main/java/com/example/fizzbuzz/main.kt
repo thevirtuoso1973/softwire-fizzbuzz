9921492427458dd73a3a802f5bc0b4df1bceaa75
Chris <Chris.Tomy@softwire.com>
2020-07-07 12:02:51 +0100

    build string instead: prints only once

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -3,66 +3,70 @@ package com.example.fizzbuzz
 import java.util.*
 
 fun main() {
+    val out = StringBuilder()
+
     val read = Scanner(System.`in`)
-    print("Enter number: ")
+    print("Enter max number (inclusive): ")
     val end: Int = read.nextInt()
+
     for (i in 1..end) {
         var some = false
         if (i % 17 == 0) {
             if (i % 11 == 0) {
-                print("Bong")
+                out.append("Bong")
                 if (i % 13 == 0) {
-                    print("Fezz")
+                    out.append("Fezz")
                 }
                 some = true
             } else {
                 if (i % 7 == 0) {
-                    print("Bang")
+                    out.append("Bang")
                     some = true
                 }
                 if (i % 5 == 0) {
-                    print("Buzz")
+                    out.append("Buzz")
                     some = true
                 }
                 if (i % 13 == 0) {
-                    print("Fezz")
+                    out.append("Fezz")
                     some = true
                 }
                 if (i % 3 == 0) {
-                    print("Fizz")
+                    out.append("Fizz")
                     some = true
                 }
             }
         } else {
             if (i % 11 == 0) {
                 if (i % 13 == 0) {
-                    print("Fezz")
+                    out.append("Fezz")
                 }
-                print("Bong")
+                out.append("Bong")
                 some = true
             } else {
                 if (i % 3 == 0) {
-                    print("Fizz")
+                    out.append("Fizz")
                     some = true
                 }
                 if (i % 13 == 0) {
-                    print("Fezz")
+                    out.append("Fezz")
                     some = true
                 }
                 if (i % 5 == 0) {
-                    print("Buzz")
+                    out.append("Buzz")
                     some = true
                 }
                 if (i % 7 == 0) {
-                    print("Bang")
+                    out.append("Bang")
                     some = true
                 }
             }
         }
         if (!some) {
-            print(i)
+            out.append(i)
         }
-        println()
+        out.append('\n')
     }
+    print(out.toString())
 }
 
