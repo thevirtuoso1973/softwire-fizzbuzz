183cdb68a0ad592851e7c0ca541be49919321b13
Chris <Chris.Tomy@softwire.com>
2020-07-07 10:24:47 +0100

    git init

---
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -0,0 +1,41 @@
+package com.example.fizzbuzz
+
+import java.util.*
+
+fun main() {
+    val read = Scanner(System.`in`)
+    print("Enter number: ")
+    val end: Int = read.nextInt()
+    for (i in 1..end) {
+        var some = false
+        if (i % 11 == 0) {
+            if (i % 13 == 0) {
+                print("Fezz")
+            }
+            print("Bong")
+            some = true
+        } else {
+            if (i % 3 == 0) {
+                print("Fizz")
+                some = true
+            }
+            if (i % 13 == 0) {
+                print("Fezz")
+                some = true
+            }
+            if (i % 5 == 0) {
+                print("Buzz")
+                some = true
+            }
+            if (i % 7 == 0) {
+                print("Bang")
+                some = true
+            }
+        }
+        if (!some) {
+            print(i)
+        }
+        println()
+    }
+}
+
